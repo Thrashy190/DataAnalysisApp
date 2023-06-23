885d5e4a3c32cc8fca6cd886639a03f05d8f55af
SmolDex <sanchezanand@gmail.com>
2023-06-23 16:16:20 -0600

    test

---
+++ src/Componentes/Cardd.jsx
@@ -0,0 +1,13 @@
+import CCard from "@coreui/react";
+import CCardBody from "@coreui/react";
+import CCardTitle from "@coreui/react";
+import CCardText from "@coreui/react";
+import CButton from "@coreui/react";
+
+const Cardd = () => {
+  return (
+<div>Bb</div>
+  );
+}
+
+export default Cardd;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,13 @@
+import Cardd from "../../Componentes/Cardd";
+
 const Login = () => {
   return (
-    <div className="bg-[#EBEDEF] h-screen">
+    <div className="bg-[#011e4a] h-screen">
       <div>Login</div>
+      <div>
+      <Cardd></Cardd>
+      </div>
+      <div>Hola mundo</div>
     </div>
   );
 };
